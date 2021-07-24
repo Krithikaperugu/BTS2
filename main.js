8b0b8ba2ceb7fc12d52d747eeacc751c2dafdd59
Krithikaperugu <81631590+Krithikaperugu@users.noreply.github.com>
2021-07-24 16:25:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+
+var audio= document.getElementById("myAudio");
+var audio= document.getElementById("myAudio2");
+var audio= document.getElementById("myAudio3");
+
+function playSound(){
+	audio.play();
+}
+
+function playSound2(){
+	audio.play2();
+}
+
+function playSound3(){
+	audio.play3();
+}
