a40256e76935e0adede4343731cc3cc3ad9256fc
Krithikaperugu <81631590+Krithikaperugu@users.noreply.github.com>
2021-07-27 17:12:25 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,31 @@
-
 var audio= document.getElementById("myAudio");
+
 var audio= document.getElementById("myAudio2");
+
 var audio= document.getElementById("myAudio3");
+
+var audio= document.getElementById("myAudio4");
+
 
 function playSound(){
 	audio.play();
 }
+ 
 
 function playSound2(){
-	audio.play2();
+	audio.pause();
+	audio.play();
 }
 
+
 function playSound3(){
-	audio.play3();
+	audio.pause();
+	audio.play();
 }
+
+
+function playSound4(){
+	audio.pause();
+	audio.play();
+}
+
